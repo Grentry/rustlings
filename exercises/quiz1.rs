0d52c360452fbf0ff7517f9d1a6fb80e364d4c57
Grentry <95578578+Grentry@users.noreply.github.com>
2024-01-09 16:18:55 +0000

    save

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(amount: i32) -> i32 {
+    if amount > 40
+    {
+        return amount;
+    }
+
+    return amount * 2;
+}
 
 // Don't modify this function!
 #[test]
